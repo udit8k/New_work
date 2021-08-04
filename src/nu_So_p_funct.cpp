8d1477e27ab15d449e5a2d170f601aca20a4628a
udit8k <87597641+udit8k@users.noreply.github.com>
2021-08-04 20:10:51 +0530

    Update nu_So_p_funct.cpp

--- src/nu_So_p_funct.cpp
+++ src/nu_So_p_funct.cpp
@@ -30,11 +30,16 @@ void nu_So_p_funct(double T, double efef, int points)
 	int plus_index =FindMinInd(arr,points);
  
 	double k_pp= kplus(int counter, double omega, int points, double energy[]);
- 	 double k_mm= kminus(int counter, double omega, int points, double energy[]);
- 		
+ 	double k_mm= kminus(int counter, double omega, int points, double energy[]);
+ 	
+ for (int i=0;i<points;i++)
+	{
+		S_o_grid[i] = 1;
+		S_o_grid_total[i]=0;
+	}	
+
  for(i=0;i<points;i++){
- 	
- 	
+ 		
 	C_mm[i]= (k[i]*k[i]+k_mm*k_mm)/(2*k_mm*k[i]);
  	C_pp[i]= (k[i]*k[i]+k_pp*k_pp)/(2*k_pp*k[i]);
 	B_mm[i]= ((1+3*C_mm[i]*C_mm[i])/2)*log(abs((1+C_mm[i])/(1-C_mm[i]))) - 3*C_mm[i];
@@ -42,36 +47,23 @@ void nu_So_p_funct(double T, double efef, int points)
 	pre_mat[i]= B_mm[i]*(N_poph(double omega, double T)+1)*(1-f0(energy_n[minus_index],efefp,T)) + B_mm[i]*(N_poph(double omega, double T)+0)*(f0(energy_n[minus_index],efefp,T))
 			+B_pp[i]*(N_poph(double omega, double T)+1)*(1-f0f0(energy_n[plus_index],efefp,T)) + B_pp[i]*(N_poph(double omega, double T)+1)*(f0f0(energy_n[plus_index],efefp,T));
 		
- 
-	//for (int aa = 0;aa<iv_number;aa++)
-	//{
-	    
-
-	    //cout<<"plus_index = "<<plus_index<<endl;
-
-//	    double f_negative = f0(energy_n[minus_index],efefp,T);
-//	    double f_positive =  f0(energy_n[plus_index],efefp,T);
-
-	    //cout<<"f_negative = "<<f_negative<<endl;
-	    //cout<<"f_positive = "<<f_positive<<endl;
 
 	if (scattering_mechanisms[1] == 1)
-		{
+{
 
-			if ((((1+3*C_mm[i]*C_mm[i])/2)*log(abs((1+C_mm[i])/(1-C_mm[i]))) - 3*C_mm[i]==0)&&(((1+3*C_pp[i]*C_pp[i])/2)*log(abs((1+C_pp[i])/(1-C_pp[i]))) - 3*C_pp[i]) ==0)
-			    S_o_grid[counter1] = average_dummy;
+	if ((((1+3*C_mm[i]*C_mm[i])/2)*log(abs((1+C_mm[i])/(1-C_mm[i]))) - 3*C_mm[i]==0)&&(((1+3*C_pp[i]*C_pp[i])/2)*log(abs((1+C_pp[i])/(1-C_pp[i]))) - 3*C_pp[i]) ==0)
+	S_o_grid[counter1] = average_dummy;
 			    // Just to avoid instability since S_o is the only denominator in g_LO and cannot be zero
-			else
-			    S_o_grid[counter1] = pre_mat[i]*(e*e*omega)/(16*pi*h_bar*v_p[i]);
+	else
+	S_o_grid[counter1] = (1/(epsilon_inf*epsilon_0)-1/(epsilon_s*epsilon_0))*pre_mat[i]*(e*e*omega)/(16*pi*h_bar*v_p[i]);
 
-
-		}
-	
-	    
-	    //cout<<"nu_iv[counter][aa] = "<<nu_iv[counter][aa]<<endl;
-	    //cout<<"nu_iv_total[counter] = "<<nu_iv_total[counter]<<endl;
-	    //getchar(); 	
-	    //} 	
-	}
 }
+ }	
+	    
+	 
+	for (int counter1=0;counter1<points;counter1++)
+        	S_o_grid_total[counter1] = S_o_grid[counter1];
+
+	
+
 }
